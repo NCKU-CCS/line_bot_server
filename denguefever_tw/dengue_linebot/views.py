db9968064bdf1933d207c2d7771db1d316f5637b
LeeW <cl87654321@gmail.com>
2016-08-31 11:36:49 +0800

    Check signature and log received request

--- denguefever_tw/dengue_linebot/views.py
+++ denguefever_tw/dengue_linebot/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+import logging
+
+from linebot.client import LineBotClient
+
+
+client = LineBotClient(**settings.LINE_BOT_SETTINGS)
+logger = logging.getLogger('django')
+
+
+@csrf_exempt
+def reply(request):
+    # Check signature
+    try:
+        if not client.validate_signature(request.META['HTTP_X_LINE_CHANNELSIGNATURE'],
+                                         request.body.decode('utf-8')):
+            logger.warning(('Invalid request to callback function.\n'
+                            'Not valid signature'
+                            'request: \n {req}').format(req=request))
+            return HttpResponseBadRequest()
+    except KeyError as ke:
+        logger.exception(('Invalid request to callback function.\n'
+                          'Does not contain X-Line-Channelsignature\n'
+                          'request: {req}\n'
+                          'exception: {e}').format(req=request,
+                                                   e=ke))
+        return HttpResponseBadRequest()
+
+    logger.info('Request Received: {}'.format(request.body))
+
+    # TODO: Reply
+    return HttpResponse()
--- denguefever_tw/denguefever_tw/urls.py
+++ denguefever_tw/denguefever_tw/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+import dengue_linebot
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^dengue_linebot/', include('dengue_linebot.urls')),
+    url(r'^callback/', dengue_linebot.views.reply),
 ]
