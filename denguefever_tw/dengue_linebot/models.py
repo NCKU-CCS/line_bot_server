d372b4d6d6333c49def2472e092a7e517e82b202
LeeW <cl87654321@gmail.com>
2016-09-05 18:14:05 +0800

    Add LINEUser to model

--- denguefever_tw/dengue_linebot/models.py
+++ denguefever_tw/dengue_linebot/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class LINEUser(models.Model):
+    user_mid = models.TextField(primary_key=True)
+    name = models.TextField()
+    picture_url = models.TextField()
+    status_message = models.TextField()
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.user_mid)
