90f10e0b7be3c0fb4972217f2adfb86dd04211d2
LeeW <cl87654321@gmail.com>
2016-09-06 11:00:07 +0800

    Make postgress host and port configrable

--- denguefever_tw/denguefever_tw/settings/production.py
+++ denguefever_tw/denguefever_tw/settings/production.py
@@ -10,7 +10,7 @@ DATABASES = {
         'NAME': get_env_variable('POSTGRESQL_NAME'),
         'USER': get_env_variable('POSTGRESQL_USER'),
         'PASSWORD': get_env_variable('POSTGRESQL_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': get_env_variable('POSTGRESQL_HOST'),
+        'PORT':  get_env_variable('POSTGRESQL_PORT'),
     }
 }
