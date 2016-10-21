9efdb94b38d675a3b0dd6af3a62af7ebeb483216
LeeW <cl87654321@gmail.com>
2016-10-21 14:46:39 +0800

    Allow LineUser status_message and pic_url to be blank

--- denguefever_tw/dengue_linebot/models.py
+++ denguefever_tw/dengue_linebot/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class LineUser(models.Model):
     user_id = models.TextField(primary_key=True)
     name = models.TextField()
-    picture_url = models.TextField()
-    status_message = models.TextField()
+    picture_url = models.TextField(blank=True)
+    status_message = models.TextField(blank=True)
 
     def __str__(self):
         return '{} ({})'.format(self.name, self.user_id)
