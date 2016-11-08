fd2c60fb91e9654f81798016ec6f767d774363aa
LeeW <cl87654321@gmail.com>
2016-11-08 18:01:11 +0800

    Update server settings

--- denguefever_tw/denguefever_tw/wsgi.py
+++ denguefever_tw/denguefever_tw/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "denguefever_tw.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "denguefever_tw.settings.local")
 
 application = get_wsgi_application()
