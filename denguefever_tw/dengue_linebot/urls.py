6548542ef5e1ed7f9d7a6a73488b68ad0f4d21f3
LeeW <cl87654321@gmail.com>
2017-01-18 17:06:56 +0800

    Add index page

--- denguefever_tw/dengue_linebot/urls.py
+++ denguefever_tw/dengue_linebot/urls.py
@@ -4,12 +4,14 @@ from .views import *
 
 
 urlpatterns = [
-    url(r'^reply/', reply),
-    url(r'^show_fsm/', show_fsm),
-    url(r'^user_list/', user_list),
-    url(r'^(?P<uid>\S+)/user_detail/', user_detail),
-    url(r'^msg_log_list/', msg_log_list),
-    url(r'^(?P<uid>\S+)/msg_log_detail/', msg_log_detail),
-    url(r'^unrecog_msgs/', unrecognized_msg_list),
-    url(r'^(?P<mid>\S+)/handle_unrecognized_msg/', handle_unrecognized_msg),
+    url(r'^reply/$', reply),
+    url(r'^$', index),
+    url(r'^show_fsm/$', show_fsm),
+    url(r'^reload_fsm/$', reload_fsm),
+    url(r'^user_list/$', user_list),
+    url(r'^(?P<uid>\S+)/user_detail/$', user_detail),
+    url(r'^msg_log_list/$', msg_log_list),
+    url(r'^(?P<uid>\S+)/msg_log_detail/$', msg_log_detail),
+    url(r'^unrecog_msgs/$', unrecognized_msg_list),
+    url(r'^(?P<mid>\S+)/handle_unrecognized_msg/$', handle_unrecognized_msg),
 ]
